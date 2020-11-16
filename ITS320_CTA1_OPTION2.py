7f52e490d0bc47dc36342792d57d00336bb80efb
tsabz <tonia.saba@gmail.com>
2020-11-16 15:41:30 -0500

    added input prompt

--- ITS320_CTA1_OPTION2.py
+++ ITS320_CTA1_OPTION2.py
@@ -1,11 +1,10 @@
 
-def option_2 (a,b):
-    print(int(a//b)) #1
-    print(float(a/b)) #1.33333333333
-    print(a % b) #1
+def option_2():
+    a = int(input("a: "))
+    b = int(input("b: "))
+    print(int(a//b))  # 1
+    print(float(a/b))  # 1.33333333333
+    print(a % b)  # 1
 
 
-option_2(4,3)
-
-
-
+option_2()
