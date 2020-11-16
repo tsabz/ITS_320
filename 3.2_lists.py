9d73f0cf1fcd2fbd8dbf3ae01492d1a59ee3b366
tsabz <tonia.saba@gmail.com>
2020-11-16 18:33:33 -0500

    Calculates the overall grade for a course with three equally-weighted exams (graded out of 100) that account for 60% of the overall grade and four equally-weighted programming assignments (graded out of 50) that account for 40% of the overall graded

--- 3.2_lists.py
+++ 3.2_lists.py
@@ -28,9 +28,36 @@ prices.append(650.00)
 prices.append(650.00)
 # print(prices)
 
-print(len(prices))  # 5
-print(min(prices))  # 76.30
-print(max(prices))  # 650.00
-print(sum(prices))  # 1203.38
-print(prices.index(76.30))  # 0
-print(prices.count(650.00))  # 2
+# print(len(prices))  # 5
+# print(min(prices))  # 76.30
+# print(max(prices))  # 650.00
+# print(sum(prices))  # 1203.38
+# print(prices.index(76.30))  # 0
+# print(prices.count(650.00))  # 2
+
+# 3.5
+exam1_grade = float(input('Enter score on Exam 1 (out of 100):\n'))
+exam2_grade = float(input('Enter score on Exam 2 (out of 100):\n'))
+exam3_grade = float(input('Enter score on Exam 3 (out of 100):\n'))
+exam4_grade = float(input('Enter score on Exam 4 (out of 100):\n'))
+exam5_grade = float(input('Enter score on Exam 5 (out of 100):\n'))
+exam6_grade = float(input('Enter score on Exam 6 (out of 100):\n'))
+exam7_grade = float(input('Enter score on Exam 7 (out of 100):\n'))
+
+
+exam1 = exam1_grade / 100
+exam2 = exam2_grade / 100
+exam3 = exam3_grade / 100
+exam4 = exam4_grade / 50
+exam5 = exam5_grade / 50
+exam6 = exam6_grade / 50
+exam7 = exam7_grade / 50
+
+sixty_percent = (exam1 + exam2 + exam3) / 3
+forty_percent = (exam4 + exam5 + exam6 + exam7) / 4
+
+
+overall_grade = ((0.6 * sixty_percent) + (0.4 * forty_percent)) * 100
+
+
+print('Your overall grade is:', overall_grade)
