ee6dbd943b1e4e7260a545135bdd71885d22536a
tsabz <tonia.saba@gmail.com>
2020-11-18 20:57:16 -0500

    car dict finish

--- ITS320_CTA2_Option2.py
+++ ITS320_CTA2_Option2.py
@@ -17,13 +17,33 @@ class Year:
     print('What is your car year: ')
     year = input()
     car['Year'] = year
+
+
+class Starting_Odometer:
+    print('What was your starting odemeter reading: ')
+    a_odometer = input()
+    car['Starting Odometer'] = a_odometer
+
+
+class Ending_Odometer:
+    print('What is your ending odometer reading: ')
+    b_Odometer = input()
+    car['Ending Odometer'] = b_Odometer
+
+
+class Miles_Per_Gallon:
+    print('What is your estimated miles per gallon: ')
+    mpg = input()
+    car['Miles Per Gallon'] = mpg + ' mpg'
     print(car)
 
 
 def main():
     Car_brand()
     Car_Model()
-    Year()
+    Starting_Odometer()
+    Ending_Odometer()
+    Miles_Per_Gallon()
 
 
 if __name__ == '__main__':
