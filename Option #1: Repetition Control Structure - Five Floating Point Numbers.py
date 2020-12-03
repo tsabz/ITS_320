fca04b4abd30131877d9c5a1e1b0b1c69f92eb5c
tsabz <tonia.saba@gmail.com>
2020-12-02 23:30:24 -0500

    able to calculate average score

--- Option #1: Repetition Control Structure - Five Floating Point Numbers.py
+++ Option #1: Repetition Control Structure - Five Floating Point Numbers.py
@@ -3,7 +3,7 @@ values_dict = {
     'Module 2': 0,
     'Module 3': 0,
     'Module 4': 0,
-    'Module 5': 0
+    'Module 5': 0,
 }
 
 # first we want to have the student enter grades into set
@@ -11,20 +11,30 @@ values_dict = {
 
 def Enter_grades():
     print('Please enter your grade for Module 1:')
-    values_dict['Module 1'] = float(input())
+    values_dict['Module 1'] = int(input())
     print('Please enter your grade for Module 2:')
-    values_dict['Module 2'] = float(input())
+    values_dict['Module 2'] = int(input())
     print('Please enter your grade for Module 3:')
-    values_dict['Module 3'] = float(input())
+    values_dict['Module 3'] = int(input())
     print('Please enter your grade for Module 4:')
-    values_dict['Module 4'] = float(input())
+    values_dict['Module 4'] = int(input())
     print('Please enter your grade for Module 5:')
-    values_dict['Module 5'] = float(input())
+    values_dict['Module 5'] = int(input())
+
+
+def Average():
+    avg = 0
+    for value in values_dict.values():
+        avg += value
+    avg = avg / len(values_dict)
+    print('Your average score is:')
+    print(avg)
 
 
 def main():
     Enter_grades()
     print(values_dict)
+    Average()
 
 
 if __name__ == "__main__":
