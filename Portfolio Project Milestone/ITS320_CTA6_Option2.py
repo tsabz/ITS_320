eb49c42cb4a90cdd8972c909540a80c7b681e858
tsabz <tonia.saba@gmail.com>
2020-12-18 18:04:54 -0500

    added notes to homework

--- Portfolio Project Milestone/ITS320_CTA6_Option2.py
+++ Portfolio Project Milestone/ITS320_CTA6_Option2.py
@@ -4,18 +4,22 @@ def cartesian():
     a_lst = []
     b_lst = []
 
+    # ask user how many elements will be in list a
     a_input = int(input("Please enter the number of elements in list a: "))
 
+    # then will alert the user if number is over 10 and ask for input again
     if a_input > 10:
         print("Please enter no more than 10 numbers")
         a_input = int(input("Please enter the number of elements in list a: "))
 
+    # will repeat steps for list b
     b_input = int(input("Please enter the number of elements in list b: "))
 
     if b_input > 10:
         print("Please enter no more than 10 numbers")
         b_input = int(input("Please enter the number of elements in list b: "))
 
+    # will loop over through the range of elements inputed in a and b to see how many times we will ask the user and then appened the numbers into list a and b
     for i in range(0, a_input):
         j = int(input("Enter number for list a: "))
         a_lst.append(j)
@@ -27,6 +31,7 @@ def cartesian():
     a = a_lst
     b = b_lst
 
+    # cartesian method, one liner for loop, looping through list a and b of the users input
     cart_list = [(i, j) for i in a for j in b]
     print(cart_list)
 
