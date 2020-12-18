e2eea40f507664a17c67038f25ab08a149208493
tsabz <tonia.saba@gmail.com>
2020-12-18 17:59:55 -0500

    refined code to ask user for input no larger than 10

--- Portfolio Project Milestone/ITS320_CTA6_Option2.py
+++ Portfolio Project Milestone/ITS320_CTA6_Option2.py
@@ -1,11 +1,31 @@
 
-
 def cartesian():
-    a_input = input("Please enter numbers: ")
-    b_input = input("Please enter numbers: ")
+    # arrays that the user inputs will be appended too
+    a_lst = []
+    b_lst = []
 
-    a = a_input.replace(" ", "")
-    b = b_input.replace(" ", "")
+    a_input = int(input("Please enter the number of elements in list a: "))
+
+    if a_input > 10:
+        print("Please enter no more than 10 numbers")
+        a_input = int(input("Please enter the number of elements in list a: "))
+
+    b_input = int(input("Please enter the number of elements in list b: "))
+
+    if b_input > 10:
+        print("Please enter no more than 10 numbers")
+        b_input = int(input("Please enter the number of elements in list b: "))
+
+    for i in range(0, a_input):
+        j = int(input("Enter number for list a: "))
+        a_lst.append(j)
+
+    for k in range(0, b_input):
+        l = int(input("Enter number for list b: "))
+        b_lst.append(l)
+
+    a = a_lst
+    b = b_lst
 
     cart_list = [(i, j) for i in a for j in b]
     print(cart_list)
