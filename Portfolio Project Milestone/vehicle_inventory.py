645b9d65c1e144a9e383d900a2684f852f16cecc
tsabz <tonia.saba@gmail.com>
2020-12-23 18:24:38 -0500

    created private variables

--- Portfolio Project Milestone/vehicle_inventory.py
+++ Portfolio Project Milestone/vehicle_inventory.py
@@ -1,1 +1,7 @@
 class Automobile:
+    def __init__(self, make, model, color, year, mileage):
+        self.__make = make
+        self.__model = model
+        self.__color = color
+        self.__year = year
+        self.__mileage = mileage
