9cf020a111301dd7767bc6c2160533d581673811
tsabz <tonia.saba@gmail.com>
2020-12-26 11:27:25 -0500

    user can now add car and attributes

--- test.py
+++ test.py
@@ -1,15 +1,31 @@
 class Automobile:
-    def __init__(self, vehicle, make):
+    def __init__(self, vehicle, make, model, color, year, mileage):
         self.__vehicle = vehicle
         self.__make = make
-        # self.__model = model
-        # self.__color = color
-        # self.__year = year
-        # self.__mileage = mileage
+        self.__model = model
+        self.__color = color
+        self.__year = int(year)
+        self.__mileage = int(mileage)
 
-    def __add_new_vehicle(self, vehicle):
-        self.__vehicle = input('What vehicle would you like to add: ')
-        vehicles['Car'] = self.__vehicle
+    def __add_new_vehicle(self, make, model, color, year, mileage):
+        self.__vehicle = input('What personal identification number would you like to give your car: ')
+        vehicles['Car'] = {}
+        self.__make = input('What is the make of your car? ')
+        vehicles['Car']['Make'] = self.__make
+        self.__model = input('What is the model of your car? ')
+        vehicles['Car']['Model'] = self.__model
+        self.__color = input('What is the color of your car? ')
+        vehicles['Car']['Color'] = self.__color
+        self.__year = input('What is the year of your car? ')
+        vehicles['Car']['Year'] = self.__year
+        self.__mileage = input('What is the mileage of your car? ')
+        vehicles['Car']['Mileage'] = self.__mileage
+
+        
+    # def __add_attributes(self, vehicle, make):
+    #     vehicles['Car'] = vehicle
+        
+    #     vehicles['Car']['Make'] = self.__make
 
     def __remove_vehicle_quest(self, vehicle):
         answer = input()
@@ -22,16 +38,15 @@ class Automobile:
     def __remove_vehicle(self, vehicle):
         del vehicles['Car']
 
-
-inventory = {}
 vehicles = {}
 car = Automobile('vehicle 1',
-                 'lexus')
+                 'lexus', 'es300', 'white', 1998, 35000)
 
 
 def main():
-    car._Automobile__add_new_vehicle('add')
+    car._Automobile__add_new_vehicle('lexus', 'es300','white', 1998, 35000)
     print(vehicles)
+    # car._Automobile__add_attributes('vehicle', 'make')
     print("Would you like to remove your vehicle?,if yes please press Y , if no please press N")
     car._Automobile__remove_vehicle_quest('delete')
     
